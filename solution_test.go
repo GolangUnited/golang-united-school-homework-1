3ceb0cc157a1ac9609acb1c635051f3a5840ac1f
korotchenkosviatoslav <99493795+korotchenkosviatoslav@users.noreply.github.com>
2022-02-11 23:59:48 +0200

    Create solution_test.go

---
+++ solution_test.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/kyokomi/emoji/v2"
+	"testing"
+)
+
+func TestHello(t *testing.T) {
+	got := Message()
+	want := emoji.Sprint("Hello :world_map:!")
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}

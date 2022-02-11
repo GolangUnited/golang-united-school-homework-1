67a9885bcb05a115b82f20e6405cf4803bdff7ac
korotchenkosviatoslav <99493795+korotchenkosviatoslav@users.noreply.github.com>
2022-02-11 23:51:32 +0200

    1

--- solution.go
+++ solution.go
@@ -1,5 +1,14 @@
-package solution
+package main
 
-func GetMessage() string {
-	return ""
+import (
+	"fmt"
+	"github.com/kyokomi/emoji/v2"
+)
+
+func Message() string {
+	Message := emoji.Sprint("Hello :world_map:!")
+	return Message
+}
+func main() {
+	fmt.Println(Message())
 }

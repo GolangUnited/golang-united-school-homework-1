7a7540439231ee33c26780aa8da55b1aae947d02
dantecyk <belyakovdima92@gmail.com>
2022-02-13 00:16:54 +0300

    new solution

--- solution.go
+++ solution.go
@@ -5,12 +5,6 @@ import (
 )
 
 func GetMessage() string {
-<<<<<<< HEAD
 	helloWorld := emoji.Sprint(":world_map:!")
 	return "hello" + helloWorld
-
-=======
-	helloWorld := emoji.Sprint(":world_map: !")
-	return "hello" + helloWorld
->>>>>>> f38b96c684bd44bb53e269a1fdb68de1e6ed256f
 }

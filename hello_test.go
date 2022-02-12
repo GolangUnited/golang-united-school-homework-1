742886867dea0e2003c9890a3c8867111ca81234
azimjonkodirov <azimjonqodirov199@gmail.com>
2022-02-12 10:48:27 +0500

    hw1

--- hello_test.go
+++ hello_test.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"testing"
-	"github.com/kyokomi/emoji/v2"
 )
 
 func TestHello(t *testing.T) {
 	got := Hello()
-	want := emoji.Sprint("Hello :world_map:!")
+	want := ("Hello 🗺️ !")
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)

0ce61e788f10e7a7b7fd1d0fef8ee228bd3e4dfa
Damir Tokmashov <damir.tokmashov@gmail.com>
2022-02-13 14:00:12 +0600

    added solution

--- solution.go
+++ solution.go
@@ -1,5 +1,13 @@
 package solution
 
+import (
+    "github.com/kyokomi/emoji/v2"
+)
+
 func GetMessage() string {
-	return ""
+    hello := "Hello"
+    world := "🗺️"
+    rendered := emoji.Sprint(hello, world)
+    rendered = rendered + "!"
+    return rendered
 }

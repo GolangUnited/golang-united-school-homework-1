3236f1dbcec1ae64a88804843fe5821b688b27ca
islam-pasha <islam.pasha-ogly@mail.ru>
2022-02-14 15:29:26 +0600

    Test

--- solution.go
+++ solution.go
@@ -1,9 +1,10 @@
 package solution
-import "github.com/kyokomi/emoji"
+
+import (
+	"github.com/kyokomi/emoji/v2"
+)
+
 func GetMessage() string {
-    hello := "Hello"
-    world := "🗺️"
-    rendered := emoji.Sprint(hello, world)
-    rendered = rendered + "!"
-    return rendered
+	returnedMsg := emoji.Sprint("Hello :world_map:!")
+	return returnedMsg
 }

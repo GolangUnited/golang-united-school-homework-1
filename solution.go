b5425923da7d644309f1b31e27a2ca92a06f9849
PoziTV <olkhovskij-dima@mail.ru>
2022-02-23 20:08:49 +0600

    02

--- solution.go
+++ solution.go
@@ -1,12 +1,15 @@
 package solution
 
 import (
+	//"io"
+	//"os"
+
 	"github.com/kyokomi/emoji"
 )
 
 func GetMessage() string {
 	const worldMap = ":world_map:"
-	out := emoji.Sprint(worldMap)
-
+	out := emoji.Sprint("Hello ", worldMap, "!")
+	//io.WriteString(os.Stdout, out)
 	return out
 }

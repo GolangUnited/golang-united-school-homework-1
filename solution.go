c51149ccb1f49c56f382c265effc09886b3dd060
zhangir11 <zhangirrr@gmail.com>
2022-06-03 18:11:16 +0600

    solved2

--- solution.go
+++ solution.go
@@ -3,5 +3,5 @@ package solution
 import "github.com/kyokomi/emoji"
 
 func GetMessage() string {
-	return "Hello " + emoji.Sprint(":world_map:") + "!"
+	return "Hello " + emoji.Sprintf(":world_map:") + "!"
 }

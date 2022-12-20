affe68f01c651c72a388162d46dfbe87f70e0d64
aslon1213 <hamidovaslon1@gmail.com>
2022-12-20 18:25:52 +0500

    First commit

--- solution.go
+++ solution.go
@@ -1,11 +1,19 @@
 package solution
 
+//package main
 import (
+	"fmt"
+
 	"github.com/kyokomi/emoji/v2"
 )
 
 func GetMessage() string {
 	message := emoji.Sprint("Hello :world_map:")
 	// ioutil.WriteFile("message.txt", []byte(message[0:len(message)-1]), 0644)
+	fmt.Println(message[0 : len(message)-1])
 	return message[0 : len(message)-1]
 }
+
+// func main() {
+// 	GetMessage()
+// }

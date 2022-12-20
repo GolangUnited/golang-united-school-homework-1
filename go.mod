141d7f3fa22ef162e129e0867421824757dcb071
aslon1213 <hamidovaslon1@gmail.com>
2022-12-20 17:57:06 +0500

    First commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module helloworld
+
+go 1.19
+
+require github.com/kyokomi/emoji/v2 v2.2.11 // indirect
--- solution.go
+++ solution.go
@@ -1,5 +1,13 @@
 package solution
 
+import (
+	"fmt"
+
+	"github.com/kyokomi/emoji/v2"
+)
+
 func GetMessage() string {
-	return ""
+	message := emoji.Sprint("Hello :world_map:!")
+	fmt.Println(message)
+	return message
 }

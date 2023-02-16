cd3cecce6cd367dbbea8b3733f3d0564c86b52f5
mukhammadkodir <qodircodermmi@gmail.com>
2023-02-17 03:31:11 +0500

    version5

--- s01.go
+++ s01.go
@@ -1,49 +1,46 @@
-package solution
+package structs
 
 import "fmt"
 
 type UserInterface interface {
-	SetFirstName(string)
-	SetLastName(string)
-	FullName() string
+  SetFirstName(string)
+  SetLastName(string)
+  FullName() string
 }
 
 type User struct {
-	firstName string
-	lastName  string
+  firstName string
+  lastName  string
 }
 
-func NewUser(lastName, firstName string) *User {
-	return &User{
-		lastName:  lastName,
-		firstName: firstName,
-	}
-}
-
-func (u *User) FullName() string {
-	return fmt.Sprintf("%s %s", u.lastName, u.firstName)
-}
-
-func (u *User) SetLastName(lastName string) {
-	u.lastName = lastName
+func New() User {
+  return User{}
 }
 
 func (u *User) SetFirstName(firstName string) {
-	u.firstName = firstName
+  u.firstName = firstName
 }
 
-func ResetUser(u *User) {
-	u.SetFirstName("")
-	u.SetLastName("")
+func (u *User) SetLastName(lastName string) {
+  u.lastName = lastName
 }
 
-func IUser(i interface{}) bool {
-	_, ok := i.(*User)
-	return ok
+func (u *User) FullName() string {
+  return u.lastName + " " + u.firstName
 }
 
-func ProcessUser(u UserInterface) string {
-	u.SetLastName("Mukhammadkodir")
-	u.SetFirstName("Mamadaliev")
-	return u.FullName()
+func ResetUser(user *User) {
+  (*user).firstName = ""
+  (*user).lastName = ""
+}
+
+func IUser(input interface{}) bool {
+  xType := fmt.Sprintf("%T", input)
+  accpectType := fmt.Sprintf("%T", User{})
+  return xType == accpectType
+}
+
+func ProcessUser(input UserInterface) string {
+  input.SetFirstName("Mukhammadkodir")
+  return input.FullName()
 }

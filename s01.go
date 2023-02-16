5b87c9e5ef300f114a7b2ad43b00e0b301bbaaae
Rakhimjon Shokirov <raximvarresult@gmail.com>
2023-02-16 11:09:29 +0500

    fix

--- s01.go
+++ s01.go
@@ -5,3 +5,38 @@ type UserInterface interface {
 	SetLastName(string)
 	FullName() string
 }
+
+type User struct {
+	firstName string
+	lastName  string
+}
+
+func New() *User {
+	return &User{}
+}
+
+func (u *User) SetFirstName(s string) {
+	u.firstName = s
+}
+
+func (u *User) SetLastName(s string) {
+	u.lastName = s
+}
+
+func (u *User) FullName() string {
+	return u.firstName + " " + u.lastName
+}
+
+func ResetUser(user UserInterface) {
+	user.SetFirstName("")
+	user.SetLastName("")
+}
+
+func IsUser(input interface{}) bool {
+	return input.(UserInterface) != nil
+}
+
+func ProcessUser(input UserInterface) string {
+	input.SetLastName("Doe")
+	return input.FullName()
+}

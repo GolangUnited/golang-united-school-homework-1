be89d3cd51dddbba44e311c22b0cd21bed5ba15d
mukhammadkodir <qodircodermmi@gmail.com>
2023-02-17 03:03:24 +0500

    In progress

---
+++ go.mod
@@ -0,0 +1,3 @@
+module golang-united-school-homework-1
+
+go 1.17
--- s01.go
+++ s01.go
@@ -1,46 +1,49 @@
-package structs
+package solution
 
 import "fmt"
 
 type UserInterface interface {
-  SetFirstName(string)
-  SetLastName(string)
-  FullName() string
+    SetFirstName(string)
+    SetLastName(string)
+    FullName() string
 }
 
 type User struct {
-  firstName string
-  lastName  string
+	firstName string
+	lastName  string
 }
 
-func New() User {
-  return User{}
-}
-
-func (u *User) SetFirstName(firstName string) {
-  u.firstName = firstName
-}
-
-func (u *User) SetLastName(lastName string) {
-  u.lastName = lastName
+func NewUser(lastName, firstName string) *User {
+	return &User{
+		lastName:  lastName,
+		firstName: firstName,
+	}
 }
 
 func (u *User) FullName() string {
-  return u.lastName + " " + u.firstName
+	return fmt.Sprintf("%s %s", u.lastName, u.firstName)
 }
 
-func ResetUser(user *User) {
-  (*user).firstName = ""
-  (*user).lastName = ""
+func (u *User) SetLastName(lastName string) {
+	u.lastName = lastName
 }
 
-func IsUser(input interface{}) bool {
-  xType := fmt.Sprintf("%T", input)
-  accpectedType := fmt.Sprintf("%T", User{})
-  return xType == accpectedType
+func (u *User) SetFirstName(firstName string) {
+	u.firstName = firstName
 }
 
-func ProcessUser(input UserInterface) string {
-  input.SetFirstName("John")
-  return input.FullName()
+func ResetUser(u *User) {
+	u.SetFirstName("")
+	u.SetLastName("")
+}
+
+func isUser(i interface{}) bool {
+	_, ok := i.(*User)
+	return ok
+}
+
+func ProcessUser(u UserInterface) string {
+	u.SetLastName("Mukhammadkodir")
+	u.SetFirstName("Mamadaliev")
+	return u.FullName()
 }

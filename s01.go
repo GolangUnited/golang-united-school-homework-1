57234851c27a90a6ec80f6f39d679634ab9346c5
QodirCoder <qodircodermmi@gmail.com>
2023-02-16 21:32:52 +0000

    Hometask

--- s01.go
+++ s01.go
@@ -1,7 +1,46 @@
 package structs
 
+import "fmt"
+
 type UserInterface interface {
-	SetFirstName(string)
-	SetLastName(string)
-	FullName() string
+  SetFirstName(string)
+  SetLastName(string)
+  FullName() string
+}
+
+type User struct {
+  firstName string
+  lastName  string
+}
+
+func New() User {
+  return User{}
+}
+
+func (u *User) SetFirstName(firstName string) {
+  u.firstName = firstName
+}
+
+func (u *User) SetLastName(lastName string) {
+  u.lastName = lastName
+}
+
+func (u *User) FullName() string {
+  return u.lastName + " " + u.firstName
+}
+
+func ResetUser(user *User) {
+  (*user).firstName = ""
+  (*user).lastName = ""
+}
+
+func IsUser(input interface{}) bool {
+  xType := fmt.Sprintf("%T", input)
+  accpectedType := fmt.Sprintf("%T", User{})
+  return xType == accpectedType
+}
+
+func ProcessUser(input UserInterface) string {
+  input.SetFirstName("John")
+  return input.FullName()
 }

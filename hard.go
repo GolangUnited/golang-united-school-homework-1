1a4555377f3259c8e62f32722f8a7a2a58eff934
Serge <rakoth.android@gmail.com>
2023-02-24 21:37:06 +0500

    hard

--- hard.go
+++ hard.go
@@ -1,29 +1,24 @@
-## IO Reader and String Task
+package hard
 
+import "strings"
+
+/*
 You own a Goal Parser that can interpret a string command.
 The command consists of an alphabet of "G", "()" and/or "(al)" in some order.
 The Goal Parser will interpret
 "G" as the string "G", "()" as the string "o", and "(al)" as the string "al".
 The interpreted strings are then concatenated in the original order.
 
-```go
-func GoalParsers(strReader *strings.Reader) string {
-panic("Not implemented yet...")
-}
-```
-
-- Task is to implement the `GoalParsers` method based on the above requirements
-
-Example Inputs:
-```
-input: G()(al)
+For example:
+input: "G()(al)
 output: Goal
-```
-```
+
 input: G()()()()(al)
 output: Gooooal
-```
-```
+
 input: (al)G(al)()()G
 output: alGalooG
-```
+*/
+func GoalParsers(strReader *strings.Reader) string {
+	panic("Not implemented yet...")
+}

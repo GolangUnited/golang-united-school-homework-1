6094f811116febd3cd1d9f36871799423318f33d
“firstname <dawitsarsenbaev@gmail.com>
2023-02-26 17:41:23 +0500

    first try

--- io_reader.go
+++ io_reader.go
@@ -1,6 +1,8 @@
 package homework
 
 import (
+	"fmt"
+	"io"
 	"strings"
 )
 
@@ -10,6 +12,10 @@ SeekTillHalfOfString -  contains a code snippet in Go that defines a function ca
 seeks to the middle of the string, reads
 half of the remaining string, and returns it as a string.
 */
+//func strReader(str string) {
+//
+//}
+
 func SeekTillHalfOfString(strReader *strings.Reader) string {
 	return ""
 }
@@ -21,6 +27,24 @@ The function takes a strings.Reader and an integer n as input,
 and splits the contents of the reader into chunks of size n.
 The function returns a slice of strings containing the chunks
 */
+
 func ReaderSplit(strReader *strings.Reader, n int) []string {
-	return []string{}
+
+	var b []string
+	reader := io.Reader(strReader)
+	buf := make([]byte, n)
+	for {
+		n, err := reader.Read(buf)
+		if err == io.EOP {
+			break
+		}
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+		if n > 0 {
+			b = append(b, string(buf[:n]))
+		}
+	}
+	return b
 }

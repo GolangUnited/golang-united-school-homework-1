c837f067a7986c5aec868e307080035ff0d2f116
“firstname <dawitsarsenbaev@gmail.com>
2023-03-02 08:57:32 +0500

    added code1

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	fmt.Println(SeekTillHalfOfString(strings.NewReader("HELLO-WORLD")))
+	//fmt.Println(ReaderSplit(strings.NewReader("HELLO-WORLD"), 2))
+}

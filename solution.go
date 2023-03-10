7659f2ff6902235cc11293d748973c265747649a
Narcolepsick1d <95755788+Narcolepsick1d@users.noreply.github.com>
2023-03-10 18:14:51 +0500

    Add files via upload

--- solution.go
+++ solution.go
@@ -1,7 +1,42 @@
-package solution
+package golang_united_school_homework_1
 
-import "github.com/kyokomi/emoji/v2"
+import (
+	"fmt"
+	"reflect"
+)
 
-func GetMessage() string {
-	return emoji.Sprint("Hello :world_map:")
+type User struct {
+	name     string
+	lastName string
+}
+type UserInterface interface {
+	SetFirstName(string)
+	SetLastName(string)
+	FullName() string
+}
+
+func (u *User) SetFirstName(name string) {
+	u.name = name
+}
+func (u *User) SetLastName(lastName string) {
+	u.lastName = lastName
+}
+func (u *User) FullName() string {
+	return fmt.Sprintf("%s %s", u.lastName, u.name)
+}
+func NewUser() User {
+	return User{}
+}
+func ResetUser(input *User) {
+	input.name = ""
+	input.lastName = ""
+}
+func IsUser(input any) bool {
+	if input == nil || reflect.TypeOf(input) != reflect.TypeOf(User{}) {
+		return false
+	}
+	return true
+}
+func ProcessUser(input UserInterface) string {
+	return input.FullName()
 }

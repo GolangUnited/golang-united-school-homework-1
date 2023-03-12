655fbf111dd84184fff28c05f55c575d8c4fe9f2
mukhammadkodir <qodircodermmi@gmail.com>
2023-03-12 20:27:43 +0500

    version8

--- io_reader.go
+++ io_reader.go
@@ -5,22 +5,12 @@ import (
 	"strings"
 )
 
-func ReaderSplit(strReader *strings.Reader, n int) []string {
-	var chunks []string
-	buf := make([]byte, n)
-	for {
-		_, err := strReader.Read(buf)
-		if err == io.EOF {
-			if len(buf) > 0 {
-				chunks = append(chunks, string(buf[:]))
-			}
-			break
-		}
-		chunks = append(chunks, string(buf[:]))
-	}
-	return chunks
-}
-
+/*
+SeekTillHalfOfString -  contains a code snippet in Go that defines a function called
+"SeekTillHalfOfString". The function takes a string reader as input,
+seeks to the middle of the string, reads
+half of the remaining string, and returns it as a string.
+*/
 func SeekTillHalfOfString(strReader *strings.Reader) string {
 	strLength := strReader.Len()
 
@@ -40,3 +30,28 @@ func SeekTillHalfOfString(strReader *strings.Reader) string {
 
 	return string(buf)
 }
+
+/*
+ReaderSplit - contains a code snippet written in Go that
+defines a function called ReaderSplit.
+The function takes a strings.Reader and an integer n as input,
+and splits the contents of the reader into chunks of size n.
+The function returns a slice of strings containing the chunks
+*/
+func ReaderSplit(strReader *strings.Reader, n int) []string {
+	var chunks []string
+	buf := make([]byte, n)
+	for {
+		_, err := strReader.Read(buf)
+		if err == io.EOF {
+			if len(buf) > 0 {
+				chunks = append(chunks, string(buf[:]))
+			}
+			break
+		}
+		chunks = append(chunks, string(buf[:]))
+	}
+	return chunks
+}
+
+
